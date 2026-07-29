93ece4a97f8b576f3f138d527d9aea59c302c9a9
MendesJ2 <109361552+MendesJ2@users.noreply.github.com>
2026-07-29 15:08:58 +0100

    Refactor InsuranceProject class structure
    Refactor InsuranceProject to inherit from BaseProject and update initialization.

--- project/project.py
+++ project/project.py
@@ -1,36 +1,13 @@
-from mltoolkit.core.base import BaseComponent
-
-from mltoolkit.data.dataset import Dataset
+from mltoolkit.project.project import BaseProject
 
 
-class BaseProject(BaseComponent):
+class InsuranceProject(BaseProject):
 
-    def __init__(self, config, logger):
+    def __init__(self,
+                 config,
+                 logger):
 
-        super().__init__(config, logger)
-
-        self.dataset = None
-
-    def load_dataset(self, dataframe):
-
-        self.dataset = Dataset(
-            dataframe,
-            config=self.config,
-            logger=self.logger,
+        super().__init__(
+            config=config,
+            logger=logger
         )
-
-        self.info("Dataset loaded.")
-
-    def summary(self):
-
-        print()
-
-        print("=" * 40)
-
-        print(self.config.project_name)
-
-        print("=" * 40)
-
-        if self.dataset:
-
-            self.dataset.summary()
