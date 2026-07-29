e607831596932a436311804168ab33f80788c792
MendesJ2 <109361552+MendesJ2@users.noreply.github.com>
2026-07-29 14:33:11 +0100

    Update logger.py

--- core/logger.py
+++ core/logger.py
@@ -1,1 +1,22 @@
+import logging
 
+
+def create_logger():
+
+    logger = logging.getLogger("MLToolkit")
+
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+
+        handler = logging.StreamHandler()
+
+        formatter = logging.Formatter(
+            "%(asctime)s | %(levelname)s | %(message)s"
+        )
+
+        handler.setFormatter(formatter)
+
+        logger.addHandler(handler)
+
+    return logger
