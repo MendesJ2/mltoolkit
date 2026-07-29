7f9bdc4e52e216559557027c42144b82a1ab5052
MendesJ2 <109361552+MendesJ2@users.noreply.github.com>
2026-07-29 16:10:58 +0100

    Modify Config class to include test_size and optional date_column
    Updated date_column to allow None values and added test_size attribute.

--- core/config.py
+++ core/config.py
@@ -1,3 +1,7 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+
+
 @dataclass
 class Config:
 
@@ -5,10 +9,12 @@ class Config:
 
     target: str
 
-    date_column: str
+    date_column: str | None = None
 
     id_columns: list[str] = field(default_factory=list)
 
     random_state: int = 42
+
+    test_size: float = 0.20
 
     output_folder: Path = Path("runs")
