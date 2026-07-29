c0b404dbafbfaa0fc3d8848a1c873acf5b504f77
MendesJ2 <109361552+MendesJ2@users.noreply.github.com>
2026-07-29 15:57:13 +0100

    Update project.py

--- project/project.py
+++ project/project.py
@@ -1,13 +1,40 @@
-from mltoolkit.project.project import BaseProject
+from mltoolkit.core.base import BaseComponent
+from mltoolkit.data.dataset import Dataset
 
 
-class InsuranceProject(BaseProject):
+class BaseProject(BaseComponent):
+    """
+    Base class for ML projects.
+    """
 
-    def __init__(self,
-                 config,
-                 logger):
+    def __init__(self, config, logger):
+        super().__init__(config=config, logger=logger)
 
-        super().__init__(
-            config=config,
-            logger=logger
+        self.dataset = None
+
+    def load_dataset(self, dataframe):
+        """
+        Load a pandas DataFrame into the project.
+        """
+
+        self.dataset = Dataset(
+            dataframe=dataframe,
+            config=self.config,
+            logger=self.logger
         )
+
+        self.info("Dataset loaded successfully.")
+
+    def summary(self):
+        """
+        Display a summary of the project.
+        """
+
+        print("=" * 50)
+        print(self.config.project_name)
+        print("=" * 50)
+
+        if self.dataset is not None:
+            self.dataset.summary()
+        else:
+            print("No dataset loaded.")
