1fb525ea04be5f85cbc23d92890e4836ea1eb603
MendesJ2 <109361552+MendesJ2@users.noreply.github.com>
2026-07-29 15:00:13 +0100

    Update feature.py

--- data/feature.py
+++ data/feature.py
@@ -1,1 +1,21 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class Feature:
+
+    name: str
+
+    dtype: str
+
+    role: str
+
+    variable_type: str
+
+    n_unique: int
+
+    missing_pct: float
+
+    is_constant: bool
+
+    is_quasi_constant: bool
