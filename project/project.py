fff951369c51523b291b35a0c19814dcad3c95f0
MendesJ2 <109361552+MendesJ2@users.noreply.github.com>
2026-07-29 14:34:21 +0100

    Implement BaseProject class with dataset loading

--- project/project.py
+++ project/project.py
@@ -1,1 +1,36 @@
+from mltoolkit.core.base import BaseComponent
 
+from mltoolkit.data.dataset import Dataset
+
+
+class BaseProject(BaseComponent):
+
+    def __init__(self, config, logger):
+
+        super().__init__(config, logger)
+
+        self.dataset = None
+
+    def load_dataset(self, dataframe):
+
+        self.dataset = Dataset(
+            dataframe,
+            config=self.config,
+            logger=self.logger,
+        )
+
+        self.info("Dataset loaded.")
+
+    def summary(self):
+
+        print()
+
+        print("=" * 40)
+
+        print(self.config.project_name)
+
+        print("=" * 40)
+
+        if self.dataset:
+
+            self.dataset.summary()
