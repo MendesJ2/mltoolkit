61c2546e6d64ade696e5d8a6719b237dd5847740
MendesJ2 <109361552+MendesJ2@users.noreply.github.com>
2026-07-30 16:39:43 +0100

    Initialize __init__.py for plots module

---
+++ eda/plots/__init__.py
@@ -0,0 +1,10 @@
+from .continuous import plot_continuous
+from .categorical import plot_categorical
+from .binary import plot_binary
+
+
+__all__ = [
+    "plot_continuous",
+    "plot_categorical",
+    "plot_binary",
+]
