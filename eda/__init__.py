665000a8c805c94aef397de553a3bd7db6fd3c39
MendesJ2 <109361552+MendesJ2@users.noreply.github.com>
2026-07-31 15:56:26 +0100

    Update __init__.py

--- eda/__init__.py
+++ eda/__init__.py
@@ -1,7 +1,9 @@
 from .analyzer import EDAAnalyzer
 from .feature import EDAFeature
+from .feature_analysis import FeatureAnalysis
 
 __all__ = [
     "EDAAnalyzer",
     "EDAFeature",
+    "FeatureAnalysis",
 ]
