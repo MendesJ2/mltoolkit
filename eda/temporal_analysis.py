29cd3e3dc428d021c3da802f2d32f86504749146
MendesJ2 <109361552+MendesJ2@users.noreply.github.com>
2026-08-03 10:14:24 +0100

    Update temporal_analysis.py

--- eda/temporal_analysis.py
+++ eda/temporal_analysis.py
@@ -1,3 +1,10 @@
+from mltoolkit.eda.plots.temporal import (
+    plot_target_temporal,
+    plot_feature_temporal,
+    plot_volume_temporal,
+)
+
+
 class TemporalAnalysis:
 
 
@@ -28,3 +35,29 @@ class TemporalAnalysis:
             "feature": self.feature_table,
 
         }
+
+
+
+    def plot_target(self):
+
+        return plot_target_temporal(
+            self.target_table,
+            self.feature_name,
+        )
+
+
+
+    def plot_feature(self):
+
+        return plot_feature_temporal(
+            self.feature_table,
+            self.feature_name,
+        )
+
+
+
+    def plot_volume(self):
+
+        return plot_volume_temporal(
+            self.target_table,
+        )
