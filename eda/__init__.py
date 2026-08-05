c1f678a6463ae4feb9786e44d868e343287dbcd2
MendesJ2 <109361552+MendesJ2@users.noreply.github.com>
2026-08-05 18:53:12 +0100

    Add EDAReport to module exports

--- eda/__init__.py
+++ eda/__init__.py
@@ -1,9 +1,12 @@
 from .analyzer import EDAAnalyzer
 from .feature import EDAFeature
 from .feature_analysis import FeatureAnalysis
+from .report import EDAReport
+
 
 __all__ = [
     "EDAAnalyzer",
     "EDAFeature",
     "FeatureAnalysis",
+    "EDAReport",
 ]
