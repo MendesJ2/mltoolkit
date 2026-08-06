ff72ab6ec43e492b25e0ea90a939d7e8681e1807
MendesJ2 <109361552+MendesJ2@users.noreply.github.com>
2026-08-06 14:31:54 +0100

    Refactor feature analysis plotting method

--- eda/feature_analysis.py
+++ eda/feature_analysis.py
@@ -1,6 +1,5 @@
 from mltoolkit.eda.plots.target import (
-    plot_target_categorical,
-    plot_target_continuous,
+    plot_target_analysis,
 )
 
 
@@ -15,46 +14,35 @@ class FeatureAnalysis:
         table,
         variable_type,
         global_rate,
+        group=None,
     ):
         self.feature_name = feature_name
         self.table = table
         self.variable_type = variable_type
         self.global_rate = global_rate
+        self.group = group
 
     def plot(self):
 
-        if self.variable_type == "continuous":
-
-            return plot_target_continuous(
-                table=self.table,
-                feature_name=self.feature_name,
-                global_rate=self.global_rate,
-            )
-
-        if self.variable_type in {
-            "binary",
-            "categorical",
-            "ordinal",
-        }:
-
-            return plot_target_categorical(
-                table=self.table,
-                feature_name=self.feature_name,
-                global_rate=self.global_rate,
-            )
-
-        raise ValueError(
-            "Target plot not implemented for variable type "
-            f"'{self.variable_type}'."
+        return plot_target_analysis(
+            table=self.table,
+            feature_name=self.feature_name,
+            variable_type=(
+                self.variable_type
+            ),
+            group=self.group,
         )
 
     def get_table(self):
+
         return self.table
 
     def __repr__(self):
+
         return (
             "FeatureAnalysis("
             f"feature_name='{self.feature_name}', "
-            f"variable_type='{self.variable_type}'"
+            f"variable_type='{self.variable_type}', "
+            f"group='{self.group}'"
             ")"
         )
