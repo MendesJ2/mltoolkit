aa282c6eb6e9310d7286c976eb4bbb2065e35706
MendesJ2 <109361552+MendesJ2@users.noreply.github.com>
2026-08-21 16:09:22 +0100

    Update __init__.py

--- modeling/__init__.py
+++ modeling/__init__.py
@@ -18,10 +18,20 @@ from .temporal_evaluation import (
     temporal_model_performance,
 )
 
+from .tree import (
+    TreeModel,
+)
+
+from .shap_evaluation import (
+    SHAPEvaluation,
+)
+
 __all__ = [
     "LogisticModel",
     "ModelEvaluation",
     "ModelComparison",
     "FeatureRelationshipStability",
     "temporal_model_performance",
+    "TreeModel",
+    "SHAPEvaluation",
 ]
